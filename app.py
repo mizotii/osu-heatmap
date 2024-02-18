3c087c67ef7d7be0ce49f966f361c6c03415163c
mizotii <mztiiiii0@gmail.com>
2024-02-17 19:22:37 -0800

    idk

--- app.py
+++ app.py
@@ -19,34 +19,6 @@ def home():
 @app.route("/authorize")
 def authorize():
     return redirect(AUTH_URL)
-    client_id = CLIENT_ID
-    redirect_uri = REDIRECT_URI
-    authorization_url = "https://osu.ppy.sh/oauth/authorize"
-    authorization_params = {
-        "client_id": CLIENT_ID,
-        "redirect_uri": REDIRECT_URI,
-        "reponse_type": "code",
-        "scope": "public identify",
-        "state": "randomval"
-    }
-    authorization_response = requests.get(authorization_url, params=authorization_params)
-    authorization_code = request.args.get("code")
-
-    token_url = "https://osu.ppy.sh/oauth/token"
-    token_data = {
-        "client_id": CLIENT_ID,
-        "client_secret": CLIENT_SECRET,
-        "code": authorization_code,
-        "grant_type": "authorization_code",
-        "redirect_uri": "http://localhost:5000"
-    }
-    token_headers = {
-        "Application": "application/json",
-        "Content-Type": "application/x-www-form-urlencoded"
-    }
-    token_response = requests.post(token_url, data=token_data, headers=token_headers)
-
-    return jsonify(token_response.json())
     
 if __name__ == "__main__":
     app.run(debug=True)
