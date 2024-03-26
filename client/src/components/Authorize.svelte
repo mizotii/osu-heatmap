6a69f576bbb36e5595454e4957339fedd640eb0e
mizotii <omphen@uw.edu>
2024-03-26 00:21:10 -0700

    init commit

---
+++ client/src/components/Authorize.svelte
@@ -0,0 +1,14 @@
+<script>
+    import { onMount } from 'svelte';
+
+    function authRedirect() {
+        
+    }
+</script>
+
+<authorize>
+    <button on:click={authRedirect}>authorize</button>
+</authorize>
+
+<style>
+</style>
---
+++ server.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify, send_from_directory
+
+app = Flask(__name__)
+
+@app.route("/")
+def base():
+    return send_from_directory('client/public', 'index.html')
+
+@app.route("/<path:path>")
+def home(path):
+    return send_from_directory('client/public', path)
+
+@app.route("/config")
+def get_config():
+    return jsonify()
+
+if __name__ == "__main__":
+    app.run(debug = True)
