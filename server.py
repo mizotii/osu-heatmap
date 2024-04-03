5cda873e77f1d21b95a850c30a0b68dc5d97499c
mizotii <omphen@uw.edu>
2024-04-03 16:53:02 -0700

    tokens can now be stored, route db todo

--- server.py
+++ server.py
@@ -1,11 +1,14 @@
-import config
 import requests
+from config import client_credentials, database, endpoints, get_headers
 from flask import Flask, jsonify, redirect, request, send_from_directory
 from flask_cors import CORS
+from models import db, User, Score
 from urllib.parse import urlencode, urljoin
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = database['DB_URI']
 CORS(app, resources={r"/config": {"origins": "http://localhost:8080"}})
+db.init_app(app)
 
 @app.route("/")
 def base():
@@ -15,52 +18,62 @@ def base():
 def home(path):
     return send_from_directory('client/public', path)
 
-@app.route("/config")
-def fetch_config():
-    try:
-        return jsonify(config.data)
-    except Exception as e:
-        return jsonify({'Error:': str(e)})
-
 @app.route("/authorize")
 def auth_redirect():
     url = create_auth_url()
     return jsonify(url)
-    
-@app.route("/callback")
-def callback():
-    try:
-        code = request.args.get('code')
-        exchange_token(code)
-        return redirect("/")
-    except Exception as e:
-        return jsonify({'Error': str(e)})
 
 def create_auth_url():
     payload = {
-        'client_id': config.data['client_credentials']['CLIENT_ID'],
-        'redirect_uri': config.data['endpoints']['REDIRECT_URI'],
+        'client_id': client_credentials['CLIENT_ID'],
+        'redirect_uri': endpoints['REDIRECT_URI'],
         'response_type': 'code',
         'scope': 'public identify',
         'state': 'randomval',
     }
     query = urlencode(payload)
-    url = urljoin(config.data['endpoints']['AUTHORIZATION_URL'], '?' + query)
+    url = urljoin(endpoints['BASE_URL'] + endpoints['AUTHORIZATION'], '?' + query)
     return url
+    
+@app.route("/callback")
+def callback():
+    try:
+        code = request.args.get('code')
+        token_data = get_token_data(code)
+        store_token(token_data)
+        return redirect("/")
+    except Exception as e:
+        return jsonify({'Error': str(e)})
 
-def exchange_token(code):
-    headers = {
-        'Accept': 'application/json',
-        'Content-Type': 'application/x-www-form-urlencoded',
-    }
-    body = {
-        'client_id': config.data['client_credentials']['CLIENT_ID'],
-        'client_secret': config.data['client_credentials']['CLIENT_SECRET'],
+def get_token_data(code):
+    payload = {
+        'client_id': client_credentials['CLIENT_ID'],
+        'client_secret': client_credentials['CLIENT_SECRET'],
         'code': code,
         'grant_type': 'authorization_code',
-        'redirect_uri': config.data['endpoints']['REDIRECT_URI'],
+        'redirect_uri': endpoints['REDIRECT_URI'],
     }
-    response = requests.post(config.data['endpoints']['TOKEN_URL'], headers=headers, data=body)
+    response = requests.post(endpoints['BASE_URL'] + endpoints['TOKEN'], headers=get_headers(), data=payload)
+    return response.json()
+
+def store_token(token_data):
+    access_token = token_data['access_token']
+    user = get_this_user(access_token)
+    db.session.add(
+        User(
+            id=user.get('id'),
+            name=user.get('username'),
+            access=access_token,
+            expires=token_data['expires_in'],
+            refresh=token_data['refresh_token'],
+            type=token_data['token_type']
+        )
+    )
+    return
+
+def get_this_user(access_token):
+    headers=get_headers(access_token)
+    response = requests.get(endpoints['BASE_URL'] + endpoints['THIS_USER'], headers=get_headers(access_token))
     return response.json()
 
 if __name__ == "__main__":
