39662c67eb4ee35edc102817c27c9e57ed42691f
mizotii <mztiiiii0@gmail.com>
2024-04-13 21:20:51 -1000

    add routes

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,7 +1,29 @@
 <script>
+	import { Router, Link, Route } from 'svelte-routing';
+
+	// components
 	import Authorize from './components/Authorize.svelte';
 	import Search from './components/Search.svelte';
+
+	//pages
+	import Error from './pages/Error.svelte';
+	import Profile from './pages/Profile.svelte';
+
+	export let url = '';
 </script>
+
+<Router {url}>
+	<nav>
+		<Link to='/'>Home</Link>
+		<Link to='/profile'>Profile</Link>
+	</nav>
+	<div>
+		<Route path='/profile/:id' let:params>
+			<Profile id='{params.id}' />
+		</Route>
+		<Route path='/error' />
+	</div>
+</Router>
 
 <main>
 	<Authorize />
