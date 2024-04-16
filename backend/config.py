b8699170c971e420287bc5a25b5e31f23bda57c6
mizotii <omphen@uw.edu>
2024-04-15 18:23:19 -0700

    refactoring

---
+++ backend/config.py
@@ -0,0 +1,51 @@
+import os
+
+client_credentials = {
+    'CLIENT_ID': os.environ.get('CLIENT_ID'),
+    'CLIENT_SECRET': os.environ.get('CLIENT_SECRET'),
+}
+
+constraints = {
+    'MAX_TOKEN_LENGTH': 2000,
+    'MAX_TYPE_LENGTH': 6,
+    'MAX_USER_LENGTH': 15,
+}
+
+database = {
+    'DB_URI': os.environ.get('DB_URI'),
+}
+
+endpoints = {
+    'BASE_URL': 'https://osu.ppy.sh/',
+    'REDIRECT_URI': 'http://localhost:5000/callback',
+    'AUTHORIZATION': 'oauth/authorize',
+    'TOKEN': 'oauth/token',
+    'THIS_USER': 'api/v2/me/',
+}
+
+errors = {
+    'USER_NOT_FOUND': 'user not found!',
+}
+
+profile = {
+    'USERNAME': 'null',
+    'RANK': 'null',
+    'SCORES': 'null',
+}
+
+user_search = {
+    'USER_FOUND': False,
+    'USER_ID': 'null',
+}
+
+def get_headers(base, token=None):
+    headers =  {
+        'Accept': 'application/json',
+    }
+    if base:
+        headers['Content-Type'] = 'application/x-www-form-urlencoded'
+    else:
+        headers['Content-Type'] = 'application/json'
+    if token:
+        headers['Authorization'] = f'Bearer {token}'
+    return headers
---
+++ backend/models.py
@@ -0,0 +1,31 @@
+from config import constraints
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import DateTime
+
+db = SQLAlchemy()
+
+def init_db(app):
+    db.init_app(app)
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(constraints['MAX_USER_LENGTH']))
+    rank = db.Column(db.Integer)
+    access = db.Column(db.String(constraints['MAX_TOKEN_LENGTH']))
+    expires = db.Column(db.Integer)
+    refresh = db.Column(db.String(constraints['MAX_TOKEN_LENGTH']))
+    type = db.Column(db.String(constraints['MAX_TYPE_LENGTH']))
+
+class Score(db.Model):
+    __tablename__ = 'scores'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    timestamp = db.Column(DateTime)
+    notes = db.Column(db.Integer)
+    accuracy = db.Column(db.Float)
+    # other stats like 300s 100s 50s misses score rank etc
+    
