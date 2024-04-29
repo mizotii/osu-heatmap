c1c7506493495e6ed6d489adaab393e1de77c55f
mizotii <mztiiiii0@gmail.com>
2024-04-29 13:02:16 -0700

    store all user rulesets properly upon initialization

--- backend/config/server_config.py
+++ backend/config/server_config.py
@@ -4,7 +4,7 @@ import pydash as _
 import requests
 import time
 from datetime import datetime, timedelta
-from models import db, Class, Score, Token, User
+from models import db, Class, Score, Token, User, UserCatch, UserMania, UserOsu, UserTaiko
 from sqlalchemy import exists, func
 from urllib.parse import urlencode, urljoin
 
@@ -24,15 +24,37 @@ endpoints = {
     'this_user': {
         'osu': '/me',
         'taiko': '/me/taiko',
-        'fruits': '/me/catch',
+        'fruits': '/me/fruits',
         'mania': '/me/mania',
     },
     'token': 'https://osu.ppy.sh/oauth/token',
 }
 
 rulesets = [
-    'osu', 'taiko', 'fruits', 'mania'
+    'fruits', 'mania', 'osu', 'taiko', 
 ]
+
+tables = {
+    'fruits': UserCatch,
+    'mania': UserMania,
+    'osu': UserOsu,
+    'taiko': UserTaiko,
+}
+
+user_attributes = {
+    'avatar_url': 'avatar_url',
+    'country_code': 'country_code',
+    'cover_url': 'cover.url',
+    'id': 'id',
+    'is_deleted': 'is_deleted',
+    'is_restricted': 'is_restricted',
+    'play_count': 'statistics.play_count',
+    'play_time': 'statistics.play_time',
+    'ranked_score': 'statistics.ranked_score',
+    'total_hits': 'statistics.total_hits',
+    'total_score': 'statistics.total_score',
+    'username': 'username',
+}
 
 # requires above config
 
@@ -43,6 +65,22 @@ authorization_parameters = {
     'scope': 'public identify',
     'state': 'randomval',
 }
+
+def after_authorization(token):
+    id = fetch_user(token['access_token'], 'osu', 'id')
+    if get_object(Token, 'user_id', id, True):
+        refresh_token(token)
+    else:
+        store_token(id, token)
+    if get_object(User, 'id', id, True):
+        update_user(token)
+    else:
+        store_user(token)
+    for ruleset in rulesets:
+        if get_object(tables[ruleset], 'id', id, True):
+            update_user_ruleset(token, ruleset)
+        else:
+            store_user_ruleset(token, ruleset)
 
 def create_authorization_url():
     return urljoin(endpoints['authorize'], '?' + urlencode(authorization_parameters))
@@ -72,26 +110,93 @@ def create_token_parameters(code):
 
 def fetch_token(code):
     response = requests.post(
-        endpoints['BASE_URL'] + endpoints['TOKEN'],
+        endpoints['token'],
         headers=create_headers(),
         data=create_token_parameters(code)
     )
     return response.json()
 
-def fetch_user(token, ruleset):
+def fetch_user(access, ruleset, attribute=None):
     if ruleset not in rulesets:
         raise ValueError(write_value_error(ruleset, rulesets))
-    response = requests.post(
+    response = requests.get(
         endpoints['api'] + endpoints['this_user'][ruleset],
-        headers=create_headers(token['access_token'])
+        headers=create_headers(access)
     )
-    return response.json()
+    data = response.json()
+    if attribute:
+        if attribute not in user_attributes:
+            raise ValueError(write_value_error(attribute, user_attributes))
+        return _.get(data, user_attributes[attribute])
+    return data
 
-# TODO
-def store_token(token):
-    user = fetch_user(token)
+def get_object(table, attribute, value, check_exists_only=False, as_dict=False):
+    valid_attributes = table.__table__.columns.keys()
+    if attribute not in valid_attributes:
+        raise ValueError(write_value_error(attribute, valid_attributes))
+    if check_exists_only:
+        return db.session.query(exists().where(getattr(table, attribute) == value)).scalar()
+    else:
+        query = (db.session.query(table).where(getattr(table, attribute) == value).first())
+        if not as_dict:
+            return query
+        else:
+            return query.as_dict()
 
-def store_user(user):
+def refresh_token(refresh):
+    return
+
+# stores token, then stores all four user rulesets
+def store_token(id, token):
+    db.session.add(
+        Token(
+            user_id=id,
+            access_token=token['access_token'],
+            expires_at=((datetime.now()+timedelta(seconds=token['expires_in']))),
+            refresh_token=token['refresh_token'],
+            token_type=token['token_type'],
+        )
+    )
+    db.session.commit()
+
+def store_user(token):
+    user = fetch_user(token['access_token'], 'osu')
+    db.session.add(
+        User(
+            avatar_url=_.get(user, user_attributes['avatar_url']),
+            country_code=_.get(user, user_attributes['country_code']),
+            cover_url=_.get(user, user_attributes['cover_url']),
+            id=_.get(user, user_attributes['id']),
+            is_deleted=_.get(user, user_attributes['is_deleted']),
+            is_restricted=_.get(user, user_attributes['is_restricted']),
+            last_updated=datetime.now(),
+            username=_.get(user, user_attributes['username']),
+        )
+    )
+    db.session.commit()
+
+def store_user_ruleset(token, ruleset):
+    user_ruleset = fetch_user(token['access_token'], ruleset)
+    db.session.add(
+        tables[ruleset](
+            id=_.get(user_ruleset, user_attributes['id']),
+            last_updated=datetime.now(),
+            username=_.get(user_ruleset, user_attributes['username']),
+            play_count=_.get(user_ruleset, user_attributes['play_count']),
+            play_time=_.get(user_ruleset, user_attributes['play_time']),
+            ranked_score=_.get(user_ruleset, user_attributes['ranked_score']),
+            streak_current=0,
+            streak_longest=0,
+            total_hits=_.get(user_ruleset, user_attributes['total_hits']),
+            total_score=_.get(user_ruleset, user_attributes['total_score']),
+        )
+    )
+    db.session.commit()
+
+def update_user(user):
+    return
+
+def update_user_ruleset(user_ruleset):
     return
 
 def write_value_error(invalid, valid):
