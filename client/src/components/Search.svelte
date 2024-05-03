4123bbc091d0a39789557e9a2f4b43f16f8a155d
mizotii <omphen@uw.edu>
2024-05-02 22:19:53 -0700

    init flexsearch

--- client/src/components/Search.svelte
+++ client/src/components/Search.svelte
@@ -1,23 +1,49 @@
-<script>
+<script lang="ts">
+    import { onMount } from "svelte";
+    import { createUsersIndex, searchUsersIndex } from "./search"
     import { navigate } from "svelte-routing";
-    import { isUserValid } from "../stores/profile";
-    import Search from "svelte-search";
 
-    let value = "";
+    let search: 'loading' | 'ready' = 'loading'
+    let searchTerm = ''
+    let results = []
 
-    async function handleSearch(username) {
-        const response = await fetch(`/api/search/${username}`);
-        const data = await response.json();
-        isUserValid.set(data.USER_FOUND);
-        if (!data.USER_FOUND) {
-            navigate(`/profile/${username}`);
-        } else {
-            navigate(`/profile/${data.USER_ID}`);
-        }
+    onMount(async() => {
+        const users = await fetch(`/api/search`).then((res) => res.json())
+        createUsersIndex(users)
+        search = 'ready'
+        console.log(users)
+    })
+
+    $: if (search === 'ready') {
+        results = searchUsersIndex(searchTerm)
     }
 </script>
 
-<Search label="field" hideLabel bind:value on:submit={handleSearch(value)} placeholder="player" />
+{#if search === 'ready'}
+    <div class='search'>
+        <input
+           bind:value={searchTerm}
+           placeholder='player...'
+           autocomplete='off'
+           spellcheck='false'
+           type='search' 
+        />
+
+        <div class='results'>
+            {#if results}
+                <ul>
+                    {#each results as result}
+                        <li>
+                            <a href='/profile/{result.id}'>
+                                {@html result.username}
+                            </a>
+                        </li>
+                    {/each}
+                </ul>
+            {/if}
+        </div>
+    </div>
+{/if}
 
 <style>
 </style>
