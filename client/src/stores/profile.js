53cf1bd84093838b5f44aedcc9540813a71f51ae
mizotii <omphen@uw.edu>
2024-05-05 22:01:36 -0700

    add writable dataType

--- client/src/stores/profile.js
+++ client/src/stores/profile.js
@@ -1,5 +1,6 @@
 import { writable } from 'svelte/store';
 
 const isUserValid = writable(false);
+const dataType = writable('note_count');
 
-export { isUserValid };
+export { dataType, isUserValid };
