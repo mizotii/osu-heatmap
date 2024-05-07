e6038ae7e06b3d02139b5ec25716d2a54b0f9d86
mizotii <omphen@uw.edu>
2024-05-06 19:10:38 -0700

    add rulesets

---
+++ client/src/components/RulesetMenu.svelte
@@ -0,0 +1,15 @@
+<script>
+    import Osu from "./rulesets/Osu.svelte";
+    import Taiko from "./rulesets/Taiko.svelte";
+    import Catch from "./rulesets/Catch.svelte";
+    import Mania from "./rulesets/Mania.svelte";
+</script>
+
+<Osu />
+<Taiko />
+<Catch />
+<Mania />
+
+<style>
+    
+</style>
