6dc2f577c8217ce4ce55582d711df6feebb3d7b4
mizotii <mztiiiii0@gmail.com>
2024-05-13 12:51:00 -0700

    handle score testing

---
+++ client/src/components/admin/SampleScore.svelte
@@ -0,0 +1,23 @@
+<script>
+    import Scores from "../scores/Scores.svelte";
+    import { onMount } from "svelte";
+
+    let scores = [];
+
+    async function getSample() {
+        const response = await fetch(`/api/scores/8816844/taiko/1715472000000`);
+        const data = await response.json();
+        console.log(data);
+        scores = data;
+    }
+
+    onMount(async () => {
+        getSample();
+    })
+</script>
+
+<Scores isHidden={false} scores={scores} ruleset={'taiko'}/>
+
+<style>
+
+</style>
