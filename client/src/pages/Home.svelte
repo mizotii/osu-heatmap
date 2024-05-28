d368966236fb09afdc7f0acccef50722ccf1a999
mizotii <omphen@uw.edu>
2024-05-27 17:04:49 -0700

    rm admin

--- client/src/pages/Home.svelte
+++ client/src/pages/Home.svelte
@@ -1,8 +1,6 @@
 <script>
     import Authorize from "../components/Authorize.svelte";
     import Search from "../components/Search.svelte";
-    import DeleteExpiredTokens from "../components/admin/DeleteExpiredTokens.svelte";
-    import QueueDailies from "../components/admin/QueueDailies.svelte";
 </script>
 
 <Authorize />
