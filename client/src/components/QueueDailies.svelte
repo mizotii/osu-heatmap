186dbfd1dc3d19e42c39928f7b45a43e372e55f2
mizotii <omphen@uw.edu>
2024-06-18 22:54:54 -0700

    init

---
+++ client/src/components/QueueDailies.svelte
@@ -0,0 +1,18 @@
+<script>
+    async function authRedirect() {
+        try {
+            const response = await fetch(`/queue_dailies`);
+            const data = await response.json();
+            window.location.href = data;
+        } catch (error) {
+            console.error('error:', error);
+        }
+    }
+</script>
+
+<authorize>
+    <button on:click={authRedirect}>queue</button>
+</authorize>
+
+<style>
+</style>
