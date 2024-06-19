68c1584a52ab7a198b4ba709a311a3f0f96aab92
mizotii <omphen@uw.edu>
2024-06-18 22:57:42 -0700

    add queue function

--- client/src/pages/Home.svelte
+++ client/src/pages/Home.svelte
@@ -1,10 +1,12 @@
 <script>
     import Authorize from "../components/Authorize.svelte";
+  import QueueDailies from "../components/QueueDailies.svelte";
     import Search from "../components/Search.svelte";
 </script>
 
 <Authorize />
 <Search />
+<QueueDailies />
 
 <style>
 
