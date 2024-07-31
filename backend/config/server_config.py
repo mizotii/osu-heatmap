4e5372beff82e0df61c6484a23b91cf85c162dcf
mizotii <omphen@uw.edu>
2024-07-30 19:34:13 -0700

    use as config

--- backend/config/server_config.py
+++ backend/config/server_config.py
@@ -6,3 +6,8 @@ from datetime import datetime, timedelta
 from models import db, Beatmap, BeatmapSet, Score, Token, User, UserCatch, UserDailyStatistics, UserMania, UserOsu, UserTaiko
 from sqlalchemy import and_, between, desc, exists
 from urllib.parse import urlencode, urljoin
+
+credentials = {
+    'client_id': os.environ.get('CLIENT_ID'),
+    'client_secret': os.environ.get('CLIENT_SECRET'),
+}
