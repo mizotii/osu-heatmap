fbe97b16b9a486063ddb0de75e763c7a4b78f625
mizotii <omphen@uw.edu>
2024-07-30 19:27:32 -0700

    add default ava, restyle

--- client/src/components/Login.svelte
+++ client/src/components/Login.svelte
@@ -1,7 +1,25 @@
 <script>
-
+    async function authRedirect() {
+        try {
+            const response = await fetch(`/authorize`);
+            const data = await response.json();
+            window.location.href = data;
+        } catch (error) {
+            console.error('error:', error);
+        }
+    }
 </script>
 
+<login>
+    <button class="btn btn-outline" on:click={authRedirect}>
+        <img class="default" src="https://s.ppy.sh/a/-1" alt="default avatar">
+        login with osu!
+    </button>
+</login>
+
 <style>
-    
+    .default {
+        position: relative;
+        max-width: 15%;
+    }
 </style>
