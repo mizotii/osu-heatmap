871a28d1e4c9337d2be1716d0a8ba95f6a418e49
mizotii <omphen@uw.edu>
2024-08-01 20:35:45 -0700

    finish

--- backend/config/authentication/callback.py
+++ backend/config/authentication/callback.py
@@ -1,14 +1,14 @@
-from server_config import credentials
+from config.server_config import credentials, endpoints, headers
+import requests
 
-endpoints = {
-    'token': 'https://osu.ppy.sh/oauth/token',
-}
-
-headers = {
-    'Accept': 'application/json',
-    'Content-Type': 'application/x-www-form-urlencoded',
-}
-
-body = {
-    'client_id': credentials['client_id'],
-}
+def request_token(code):
+    url = endpoints['token']
+    payload = {
+        'client_id': credentials['client_id'],
+        'client_secret': credentials['client_secret'],
+        'code': code,
+        'grant_type': 'authorization_code',
+        'redirect_uri': endpoints['callback']
+    }
+    r = requests.post(url, headers=headers, data=payload)
+    return r.json()
