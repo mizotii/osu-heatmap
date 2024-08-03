5c0fb71d7eb0d259ccfccbe60319f1e46d40c62e
mizotii <omphen@uw.edu>
2024-08-02 21:51:15 -0700

    fully implement login

--- client/src/components/Login.svelte
+++ client/src/components/Login.svelte
@@ -1,4 +1,18 @@
 <script>
+    import { onMount } from "svelte";
+
+    let isAuthenticated = false;
+    let username;
+    let avatar_url;
+
+    async function fetchUser() {
+        const response = await fetch(`api/get_user_data`);
+        const data = await response.json();
+        console.log(data);
+        username = data['username'];
+        avatar_url = data['avatar_url'];
+    }
+
     async function authRedirect() {
         try {
             const response = await fetch(`/authorize`);
@@ -8,12 +22,39 @@
             console.error('error:', error);
         }
     }
+
+    onMount(() => {
+        fetch(`/api/get_session`, {
+            credentials: 'include',
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            if (data.login == true) {
+                isAuthenticated = true;
+                fetchUser();
+            } else {
+                isAuthenticated = false;
+            }
+            console.log(isAuthenticated);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    });
+
+
 </script>
 
 <login>
     <button class="btn btn-outline" on:click={authRedirect}>
-        <img class="default" src="https://s.ppy.sh/a/-1" alt="default avatar">
-        login with osu!
+        {#if isAuthenticated}
+            <img class="default" src="{avatar_url}" alt="default avatar">
+            {username}
+        {:else}
+            <img class="default" src="https://s.ppy.sh/a/-1" alt="default avatar">
+            login with osu!
+        {/if}
     </button>
 </login>
 
