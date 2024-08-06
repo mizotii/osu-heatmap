6da9d36fe66acbc2bd65fa7b260ee3efe419c471
mizotii <omphen@uw.edu>
2024-08-05 18:36:39 -0700

    break ground on updating user rulesets

--- backend/db/update.py
+++ backend/db/update.py
@@ -4,11 +4,16 @@ from datetime import datetime, timedelta
 from db.models import db, User
 from config.osu_api import fetch as ft
 
+user_ruleset_attributes = [
+    'last_updated',
+    'username',
+]
+
 def store_user(token, user):
     db.session.add(User(
         id=user['id'],
         access_token=token['access_token'],
-        expires_at=timedelta(seconds = token['expires_in'])+datetime.now(),
+        expires_at=timedelta(seconds=token['expires_in'])+datetime.now(),
         refresh_token=token['refresh_token'],
         token_type=token['token_type'],
 
@@ -24,3 +29,9 @@ def store_user(token, user):
     ))
     db.session.commit()
     return
+
+def update_user_ruleset(app, user, ruleset):
+    with app.app_context():
+        updated_user = ft.fetch_user(user.__dict__['access_token']).__dict__
+        # todo: update ruleset attributes for everything except streaks using collection above
+        return
