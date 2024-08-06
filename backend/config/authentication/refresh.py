1034878256e94008b8429a75ee0f42c6e34d8505
mizotii <omphen@uw.edu>
2024-08-05 18:35:55 -0700

    implement token refresher

--- backend/config/authentication/refresh.py
+++ backend/config/authentication/refresh.py
@@ -1,14 +1,18 @@
 from config.server_config import credentials, endpoints, headers
+from datetime import datetime, timedelta
 import requests
 
-def refresh_token(user):
-    url = endpoints['token']
-    payload = {
-        'client_id': credentials['client_id'],
-        'client_secret': credentials['client_secret'],
-        'grant_type': 'refresh_token',
-        'refresh_token': user.__dict__['refresh_token'],
-    }
-    r = requests.post(url=url, headers=headers, params=payload)
-    
-    return
+def refresh_token(app, user):
+    with app.app_context():
+        url = endpoints['token']
+        payload = {
+            'client_id': credentials['client_id'],
+            'client_secret': credentials['client_secret'],
+            'grant_type': 'refresh_token',
+            'refresh_token': user.__dict__['refresh_token'],
+        }
+        r = requests.post(url=url, headers=headers, params=payload).json()
+        setattr(user, 'access_token', r['access_token'])
+        setattr(user, 'expires_at', timedelta(seconds = r['expires_in']) + datetime.now())
+        setattr(user, 'refresh_token', r['refresh_token'])
+        return
