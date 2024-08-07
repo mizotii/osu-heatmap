48eb082805327419c82e3a877502e3a036a7f704
mizotii <mztiiiii0@gmail.com>
2024-08-07 16:07:13 -0700

    add score fetcher

--- backend/config/osu_api/fetch.py
+++ backend/config/osu_api/fetch.py
@@ -1,4 +1,5 @@
 from config.server_config import endpoints, headers
+from urllib.parse import urlencode, urljoin
 import requests
 
 # get User data
@@ -9,4 +10,16 @@ def fetch_user(access, ruleset=None):
         r = requests.get(endpoints['v2'] + endpoints[ruleset], headers=headers)
     else:
         r = requests.get(endpoints['v2'] + endpoints['user'], headers=headers)
+    return r.json()
+
+def fetch_scores(access, id, ruleset):
+    auth = 'Bearer {}'
+    headers['Authorization'] = auth.format(access)
+    endpoint = f'/users/{id}/scores/recent'
+    query_params = {
+        'include_fails': 1,
+        'mode': ruleset,
+        'limit': 999,
+    }
+    r = requests.get(endpoints['v2'] + endpoint, headers=headers, params=query_params)
     return r.json()
