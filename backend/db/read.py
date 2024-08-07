bfa40b29f1aab673c2bc6c3e88fa5e473eb3fc1f
mizotii <omphen@uw.edu>
2024-08-06 20:39:45 -0700

    implement querying of rulesets and heatmap cells

--- backend/db/read.py
+++ backend/db/read.py
@@ -1,9 +1,24 @@
 """for reading from the database"""
-from db.models import db, User
+from db.models import db, User, UserOsu, UserTaiko, UserCatch, UserMania, UserDailyStatistics
+
+ruleset_tables = {
+    'osu': UserOsu,
+    'taiko': UserTaiko,
+    'catch': UserCatch,
+    'mania': UserMania,
+}
 
 def read_user(id):
     user = User.query.filter_by(id=id).first()
     return user
+
+def read_ruleset(id, ruleset):
+    user = ruleset_tables[ruleset].query.filter_by(id=id).first()
+    return user
+
+def read_cell(id, ruleset, date):
+    cell = UserDailyStatistics.query.filter_by(id=id, ruleset=ruleset, start_date=date).first()
+    return cell
 
 def all_users(app):
     with app.app_context():
