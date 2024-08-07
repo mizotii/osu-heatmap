d23a30ee766f7bdeca3f0bfd3a3f8578b7ae19e4
mizotii <omphen@uw.edu>
2024-08-06 20:38:17 -0700

    add ruleset route

--- backend/config/osu_api/fetch.py
+++ backend/config/osu_api/fetch.py
@@ -2,8 +2,11 @@ from config.server_config import endpoints, headers
 import requests
 
 # get User data
-def fetch_user(access):
+def fetch_user(access, ruleset=None):
     auth = 'Bearer {}'
     headers['Authorization'] = auth.format(access)
-    r = requests.get(endpoints['v2'] + endpoints['user'], headers=headers)
+    if ruleset:
+        r = requests.get(endpoints['v2'] + endpoints[ruleset], headers=headers)
+    else:
+        r = requests.get(endpoints['v2'] + endpoints['user'], headers=headers)
     return r.json()
