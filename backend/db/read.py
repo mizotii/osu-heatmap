33715842c3a82022427827eefeb1371d79e0a9bd
mizotii <mztiiiii0@gmail.com>
2024-08-07 16:07:57 -0700

    allow reading of scores, beatmaps, and beatmapsets

--- backend/db/read.py
+++ backend/db/read.py
@@ -1,5 +1,5 @@
 """for reading from the database"""
-from db.models import db, User, UserOsu, UserTaiko, UserCatch, UserMania, UserDailyStatistics
+from db.models import db, User, UserOsu, UserTaiko, UserCatch, UserMania, UserDailyStatistics, Score, Beatmap, BeatmapSet
 from config import server_config as sc
 
 def read_user(id):
@@ -13,6 +13,18 @@ def read_ruleset(id, ruleset):
 def read_cell(id, ruleset, date):
     cell = UserDailyStatistics.query.filter_by(id=id, ruleset=ruleset, start_date=date).first()
     return cell
+
+def read_beatmap(id, set_id):
+    beatmap = Beatmap.query.filter_by(id=id, beatmapset_id=set_id).first()
+    return beatmap
+
+def read_beatmapset(id):
+    beatmapset = BeatmapSet.query.filter_by(id=id).first()
+    return beatmapset
+
+def read_score(id):
+    score = Score.query.filter_by(id=id).first()
+    return score
 
 def all_users(app):
     with app.app_context():
