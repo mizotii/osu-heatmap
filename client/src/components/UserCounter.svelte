4cd2d6ba226b74e3914687a88d6566738132781b
mizotii <omphen@uw.edu>
2024-08-11 22:26:08 -0700

    add backend api

--- client/src/components/UserCounter.svelte
+++ client/src/components/UserCounter.svelte
@@ -1,10 +1,12 @@
 <script>
     import { onMount } from "svelte";
 
+    const apiEndpoint = process.env.BACKEND_API;
+
     let users;
 
     async function getUserCount() {
-        const response = await fetch(`/api/get_user_count`);
+        const response = await fetch(`${apiEndpoint}/api/get_user_count`);
         const data = await response.json();
         userCountString(data.count);
     }
