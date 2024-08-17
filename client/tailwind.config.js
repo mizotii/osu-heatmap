53d04d2128563dd3772cb6d3150a8b7c5acc96b7
mizotii <omphen@uw.edu>
2024-08-17 16:13:26 -0700

    theme overhaul

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -8,6 +8,22 @@ module.exports = {
     require("daisyui")
   ],
   daisyui: {
-    themes: ["business"],
+    themes: [
+      {
+        osuheatmap: {
+          "primary": "#ffc0d3",
+          "primary-content": "#fff",
+          "secondary": "#ce93d8",          
+          "secondary-content": "#fff",
+          "accent": "#ffc0d3",
+          "accent-content": "#fff",
+          "neutral-content": "#fff",
+          "base-100": "#333333",
+          "base-200": "#222222",
+          "base-300": "#111111",
+          "base-content": "#fff",
+        },
+      },
+    ],
   },
 }
