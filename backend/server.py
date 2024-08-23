b35711c184d2418f973a7e7ac2a161dce5c881a3
mizotii <omphen@uw.edu>
2024-08-22 17:57:47 -0700

    debug refresher

--- backend/server.py
+++ backend/server.py
@@ -157,7 +157,7 @@ def auto_update():
         up.update_user_statistics(app, user)
 
 logging.basicConfig(level=logging.INFO)
-scheduler.add_job(auto_update, 'interval', seconds=30)
+scheduler.add_job(auto_update, 'interval', minutes=15)
 scheduler.start()
 scheduler.print_jobs()
 
