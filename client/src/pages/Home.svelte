c36d5d6244c92a063213382991d6d181f3cec378
mizotii <omphen@uw.edu>
2024-08-23 18:00:57 -0700

    add info

--- client/src/pages/Home.svelte
+++ client/src/pages/Home.svelte
@@ -3,7 +3,13 @@
 </script>
 
 <Content>
-    
+    <article class="prose max-w-none">
+        <h2 class='text-center'>osu-heatmap! - A personal statistic tracker for osu!</h2>
+        <h3>What is this?</h3>
+        <p>osu-heatmap! is nothing more than a calendar heatmap which allows you to visualize the effort you put into the game. It does so in a manner that we hope you find motivating or more meaningful!</p>
+        <h3>OK, cool. Sign me up!</h3>
+        <p>All you have to do is authorize yourself, and we take care of the rest. Users are queued every day at midnight in the time zone they signed up in, with scores being checked both routinely and upon a search.</p>
+    </article>
 </Content>
 
 <style>
