97088046bfea168e48eb621264b876a7830acb11
mizotii <omphen@uw.edu>
2024-08-26 21:55:44 -0700

    serve demo file

--- client/src/pages/Home.svelte
+++ client/src/pages/Home.svelte
@@ -9,7 +9,7 @@
         <p>osu-heatmap! is nothing more than a calendar heatmap which allows you to visualize the efforts you put into the game. It does so in a manner that we hope you find motivating or more meaningful than a simple playcount graph!</p>
         <h3>What does it look like?</h3>
         <p>As of now, it looks something like this.</p>
-        <img src='../public/static/demo.png' alt='osu-heatmap demo' width='600'>
+        <img src='/static/demo.png' alt='osu-heatmap demo' width='600'>
         <p>We also plan on adding customizable themes and BBCode signatures that you can add to your osu! profiles in the future!</p>
         <h3>OK, cool. Sign me up!</h3>
         <p>All you have to do is authorize yourself by clicking the avatar in the top left corner, and we take care of the rest. Users are queued frequently enough that you should be able to see your gameplay logged in close to real time.</p>
