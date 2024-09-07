221ce6bb7e1544f7c357f3d760ae73424aa82b14
mizotii <omphen@uw.edu>
2024-09-07 15:27:08 -0700

    work on score reformat

--- client/src/components/SampleScore.svelte
+++ client/src/components/SampleScore.svelte
@@ -2,10 +2,11 @@
 
 </script>
 
-<div class="container container-lg w-9/10 h-12 m-4 flex flex-column blur-sm brightness-[0.33]" style="background-image: url('https://assets.ppy.sh/beatmaps/1155295/covers/slimcover@2x.jpg?1650694601')">
+<div class="container container-lg relative w-9/10 h-12 m-4 border border-white flex flex-column">
+    <img src='https://assets.ppy.sh/beatmaps/1155295/covers/slimcover@2x.jpg?1650694601' class='blur-sm brightness-[0.33]'>
     <div class='flex flex-row basis-4/5'>
         <div class='basis-3/5'>
-        
+            Duplicity Shade [[7K] Metamorphosis]
         </div>
         <div class='basis-2/5'>
         
