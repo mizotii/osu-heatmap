fd4e31edbaa99b67bed15705fe2be941db8228a3
mizotii <omphen@uw.edu>
2024-09-08 16:04:08 -0700

    formatting

--- client/src/components/SampleScore.svelte
+++ client/src/components/SampleScore.svelte
@@ -2,21 +2,26 @@
 
 </script>
 
-<div class="container container-lg relative w-9/10 h-12 m-4 border border-white flex flex-column">
-    <img src='https://assets.ppy.sh/beatmaps/1155295/covers/slimcover@2x.jpg?1650694601' class='blur-sm brightness-[0.33]'>
-    <div class='flex flex-row basis-4/5'>
-        <div class='basis-3/5'>
-            Duplicity Shade [[7K] Metamorphosis]
-        </div>
-        <div class='basis-2/5'>
-        
-        </div>
+<div class="container-lg flex-row container relative m-4 flex h-16 border border-white text-white" style="background-image: url('https://assets.ppy.sh/beatmaps/1155295/covers/slimcover@2x.jpg?1650694601');">
+  <div class='flex flex-col basis-4/5 backdrop-blur-sm backdrop-brightness-50'>
+    <div class='basis-3/5 font-bold text-lg'>
+      Duplicity Shade [[7K] Metamorphosis]
     </div>
-    <div class='basis-1/5'>
-        <div class='text-[10px]'>
-            5737 / 3722 / 719 / 241 / 102 / 307
-        </div>
+    <div class='basis-2/5 text-[10px]'>
+      <span>HyuN feat. Sennzai</span>
+      <span class='text-gray-300'>
+        Sun, 08 Sep 2024 06:13:48 GMT
+      </span>
     </div>
+  </div>
+  <div class='basis-1/5 backdrop-blur-sm backdrop-brightness-50 text-center'>
+    <div class='font-bold'>
+      score: +999999
+    </div>
+    <div class='text-[8px]'>
+      5737 / 3722 / 719 / 241 / 102 / 307
+    </div>
+  </div>
 </div>
 
 <style>
