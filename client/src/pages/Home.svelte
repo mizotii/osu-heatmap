0008ba4d035c375beb1e651bf88b4af69646d003
mizotii <omphen@uw.edu>
2024-10-16 12:03:05 -0700

    make demo clickable

--- client/src/pages/Home.svelte
+++ client/src/pages/Home.svelte
@@ -16,7 +16,9 @@
         <h3>What is this?</h3>
         <p>osu-heatmap! is a calendar heatmap which allows you to visualize all the efforts you've put into the game! It does so in a manner that we hope you find motivating or more meaningful than a simple playcount graph.</p>
         <h3>What does it look like?</h3>
-        <img src='/static/demo.png' alt='osu-heatmap demo' width='600'>
+        <a href='https://osu-heatm.app/profile/8816844'>
+            <img src='/static/demo.png' alt='osu-heatmap demo' width='600'>
+        </a>
         <p>We also plan on adding customizable themes and BBCode signatures that you can add to your osu! profiles in the future!</p>
         <h3>OK, cool. Sign me up!</h3>
         <p>All you have to do is authorize yourself by clicking the avatar in the top left corner, and we take care of the rest. Users are queued frequently enough that you should be able to see your gameplay logged in close to real time.</p>
