d192fb99dc52d864a54b64a86b14684bd2db3310
mizotii <omphen@uw.edu>
2024-11-28 17:46:37 -0800

    init

---
+++ client/src/components/Rank.svelte
@@ -0,0 +1,14 @@
+<script>
+    import { rank } from "../../config.json";
+
+    export let grade;
+</script>
+
+<rank>
+    <div class='text-[{rank[grade]}]'>
+        {grade}
+    </div>
+</rank>
+
+<style>
+</style>
